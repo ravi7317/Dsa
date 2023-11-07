db4d60c4ba6756e7d06d0fe43d3d9648d2e4a88d
Ravi Verma <+>
2023-11-07 15:11:01 +0530

    added my first c++ code

---
+++ arrayday1.cpp
@@ -0,0 +1,19 @@
+// C++ program to demonstrate the taking 
+// multiple inputs from the user 
+#include <iostream> 
+using namespace std; 
+
+// Driver Code 
+int main() 
+{ 
+	string name; 
+	int age; 
+
+	
+	cin >> name >> age; 
+
+	cout << "Name : " << name << endl; 
+	cout << "Age : " << age << endl; 
+
+	return 0; 
+}
