21c02302b1fcb36a5e8ec4b2bc72757a4511fe0e
Ravi Verma <+>
2023-11-08 00:40:47 +0530

    Added

---
+++ day1/compoundstatement.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int sum=10;
+    int x=5;
+    sum+=x; //you can right both side sum otherwise insted of both side 
+    //dont right both side sum u can use sum+ and for all like multiplication division and substaction 
+    // sum+, sum* ,sum/ ,sum-
+    cout<<sum;
+    return 0;
+}
