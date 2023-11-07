eff60f327fbeff3f0f9095f83d339fb24106483e
Ravi Verma <+>
2023-11-07 23:29:29 +0530

    added

---
+++ day1/Areaoftraingle.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    float b,h ,a;
+    cout<<"Enter the are of bash and height "<<endl;
+    cin>>b>>h;
+ a=(b*h)/2;
+ cout<<"the area of traingle is "<<a;
+    return 0;
+}
