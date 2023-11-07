e4a5fbeaed785f11c7cd6555b4c9574ca030c477
Ravi Verma <+>
2023-11-08 00:10:22 +0530

    Added

---
+++ day1/radiusof-circle.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    float r,area;
+    cout<<"radius of circle"<<endl;
+    cin>>r;
+    area=3.142f*r*r;
+    cout<<"Area is "<<area;
+    return 0;
+}
